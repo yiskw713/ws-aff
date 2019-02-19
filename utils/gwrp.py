3ce47b6c0b952399e859ba58b3b9f08b74dd841d
yuchi ishikawa <yuchi@yuchinobookpuro.dhcp.hakozaki.ibm.com>
2019-02-19 16:53:44 +0900

    first commit

--- utils/gwrp.py
+++ utils/gwrp.py
@@ -1,17 +1,35 @@
 import torch
 
 
-def global_weighted_rank_pooling(feats, d=0.996):
+# def global_weighted_rank_pooling(feats, d=0.996):
+#     n, c, h, w = feats.shape
+
+#     desc_inds = torch.argsort(
+#         feats.reshape(n, c, -1), dim=2, descending=True)   # (n, c, h*w)
+#     ds = torch.zeros_like(desc_inds, dtype=torch.float) + d    # (n, c, h*w)
+#     nn, cc, inds = torch.meshgrid(
+#         torch.arange(n), torch.arange(c), torch.arange(h * w)
+#     )
+#     weights = torch.pow(ds, inds.float())    # (n, c, h*w)
+#     z_dc = torch.sum(weights[0, 0])    # (1,)
+#     desc_feats = feats.reshape(n, c, -1)[nn, cc, desc_inds]
+#     y_gwrp = torch.sum(weights * desc_feats / z_dc, dim=2)
+#     return y_gwrp
+
+
+def global_weighted_rank_pooling(feats, label, d_p=0.996, d_m=0., d_bg=0.999):
     n, c, h, w = feats.shape
 
     desc_inds = torch.argsort(
         feats.reshape(n, c, -1), dim=2, descending=True)   # (n, c, h*w)
-    ds = torch.zeros_like(desc_inds, dtype=torch.float) + d    # (n, c, h*w)
+    ds = torch.zeros_like(desc_inds, dtype=torch.float) + d_p    # (n, c, h*w)
+    ds[label.nonzero()[:, 0], label.nonzero()[:, 1]] = d_m
+    ds[:, 0] = d_bg
     nn, cc, inds = torch.meshgrid(
         torch.arange(n), torch.arange(c), torch.arange(h * w)
     )
     weights = torch.pow(ds, inds.float())    # (n, c, h*w)
-    z_dc = torch.sum(weights[0, 0])    # (h*w,)
+    z_dc = torch.sum(weights, dim=2, keepdim=True)    # (n, 1, h*w)
     desc_feats = feats.reshape(n, c, -1)[nn, cc, desc_inds]
     y_gwrp = torch.sum(weights * desc_feats / z_dc, dim=2)
     return y_gwrp
---
+++ utils/loss.py
@@ -0,0 +1,73 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+from .crf import DenseCRF
+from .gwrp import global_weighted_rank_pooling
+from joblib import Parallel, delayed
+
+
+class ExpansionLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, seg_out, label):
+        prob = torch.softmax(seg_out, dim=1)
+        y_gwrp = global_weighted_rank_pooling(prob, label)
+
+        pos = label.nonzero()
+        neg = (label == 0.).nonzero()
+        p, _ = pos.shape
+        n, _ = neg.shape
+
+        pos_loss = - torch.log(y_gwrp[pos[:, 0], pos[:, 1]]) / p
+        neg_loss = - torch.log(1 - y_gwrp[neg[:, 0], neg[:, 1]]) / n
+
+        return pos_loss + neg_loss
+
+
+class SeedingLoss(nn.Module):
+    def __init__(self, threshold=0.2):
+        super().__init__()
+        self.threshold = threshold
+        self.criterion = nn.CrossEntropyLoss(ignore_index=-100)
+
+    def forward(self, seg_out, cam):
+        # the values of cam after normalized. shape => (N, C, H', W')
+        # ignore all classes images do not have (-100)
+        cam = torch.where(cam > 0.2, torch.tensr(1), torch.tensor(-100))
+        loss = self.loss(seg_out, cam)
+
+        return loss
+
+
+class ConstrainToBoundaryLoss(nn.Module):
+    def __init__(self, device):
+        super().__init__()
+        self.crf = DenseCRF()
+        self.device = device
+
+    def forward(self, img, seg_out, label):
+        # downscale image
+        _, _, h, w = seg_out.shape
+        img = F.interpolate(img, (h, w), mode='bilinear')
+        # image: (N, 3, h, w) -> (N, h, w, 3)
+        img = (img * 255).to('cpu').numpy().astype(np.uint8).transpose(0, 2, 3, 1)
+        prob = torch.softmax(seg_out)    # shape => (N, C, h, w)
+        probmap = prob.numpy()
+
+        # CRF
+        Q = Parallel(n_jobs=-1)([
+            delayed(self.crf)(*pair) for pair in zip(img, probmap)
+        ])
+        Q = torch.tensor(Q).to(self.device)    # shape => (N, C, h, w)
+
+        # ignore all the classes except classes in images
+        neg = (label == 0.).nonzero()
+        Q[neg[:, 0], neg[:, 1]] = 0.
+
+        # the number of pixels of classes which images have
+        num = label.sum() * h * w
+        log = torch.log((Q + 1e-7) / prob)
+        loss = torch.sum(Q * log) / num
+        return loss
